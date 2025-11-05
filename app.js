b9fe51efbb0905d467f6752dcfed32b02ea3af1f
Suman Kumar <sumankumar36653@gmail.com>
2025-11-06 03:39:28 +0530

    fixed login logut problems

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
-};
+}
 
 const express = require("express");
-const app = express({ mergeParams: true });
-const port = 8080;
+const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
@@ -15,76 +14,69 @@ const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User = require("./models/user.js")
+const User = require("./models/user.js");
 
-
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const dbUrl = process.env.ATLASDB_URL;
 
-
-const listingsRouter = require("./routes/listing.js");
-const reviewsRouter = require("./routes/review.js");
-const userRouter = require("./routes/user.js");
-
-
-
-main().then(() => {
-    console.log('connected to db');
-})
+main()
+    .then(() => {
+        console.log("connected to DB");
+    })
     .catch((err) => {
         console.log(err);
-    })
+    });
 
 async function main() {
     await mongoose.connect(dbUrl);
 }
 
-app.engine("ejs", ejsMate);
-app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
-app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto: {
         secret: process.env.SECRET,
     },
-    touchAfter: 24 * 3600 // time period in seconds
-})
+    touchAfter: 24 * 3600,
+});
 
-store.on("error", function (e) {
-    console.log("SESSION STORE ERROR", e)
+store.on("error", () => {
+    console.log("ERROR in MONGO SESSION STORE", err);
 });
 
 const sessionOptions = {
-    store: store,
+    store,
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        secure: false // ensure false on localhost
-    }
+    },
 };
 
-
-// app.get("/", (req, res) => {
-//     res.send("this is root ")
-
-// });
+// Root route redirects to listings
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
-app.use(flash())
+app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -93,26 +85,22 @@ app.use((req, res, next) => {
     res.locals.error = req.flash("error");
     res.locals.currentUser = req.user;
     next();
-})
-
-
-app.use("/listings", listingsRouter);
-app.use("/listings/:id/reviews", reviewsRouter);
-app.use("/", userRouter);
-
-
-// Wrong route handler
-app.use((req, res, next) => {
-    next(new ExpressError(404, "Page Not found"));
 });
 
-// Error-handling middleware
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
+
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"));
+});
+
 app.use((err, req, res, next) => {
-    const { statusCode = 500, message = "Something went wrong" } = err;
+    let { statusCode = 500, message = "Something went wrong!" } = err;
     res.status(statusCode).render("error.ejs", { message });
 });
 
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`server running at: http://localhost:${port}`);
-
-})
+    console.log(`Server is listening at http://localhost:${port}`);
+});
--- routes/user.js
+++ routes/user.js
@@ -1,29 +1,57 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
-const wrapAsync = require("../utils/wrapAsync");
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
-const { saveRedirectUrl } = require("../middlewares");
-const userController = require("../controllers/users.js");
+const { saveRedirectUrl } = require("../middlewares.js");
 
-// Signup route 
-router.route("/signup")
-  .get(userController.renderSignupForm)
-  .post(wrapAsync(userController.signup));
+router.get("/signup", (req, res) => {
+    res.render("users/signup.ejs");
+});
 
-//login route and authentication route
-router.route("/login")
-  .get(userController.renderLoginForm)
-  .post(
-    saveRedirectUrl, 
-    passport.authenticate("local", { 
-      failureFlash: true, 
-      failureRedirect: "/login" 
-    }), 
-    userController.login
-  );
+router.post("/signup", wrapAsync(async (req, res, next) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+        const registeredUser = await User.register(newUser, password);
+        req.login(registeredUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to GharYatra!");
+            res.redirect("/listings");
+        });
+    } catch (e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+}));
 
-// Logout route
-router.post("/logout", userController.logout);
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+router.post("/login",
+    saveRedirectUrl,
+    passport.authenticate("local", {
+        failureRedirect: "/login",
+        failureFlash: true
+    }),
+    async (req, res) => {
+        req.flash("success", "Welcome back to GharYatra!");
+        let redirectUrl = res.locals.redirectUrl || "/listings";
+        res.redirect(redirectUrl);
+    }
+);
+
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "You are logged out!");
+        res.redirect("/listings");
+    });
+});
 
 module.exports = router;
